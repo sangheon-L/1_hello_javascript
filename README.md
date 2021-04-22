65a021ef16125ed9acb05f050e67da53f471077c
snagheon-L <tkdgjs0107@naver.com>
2021-04-22 17:01:07 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 1_hello_javascript
+ 4.5_javascript
---
+++ js/common.js
@@ -0,0 +1,49 @@
+// alert('hello javascript');
+// console.log('hello javascript');
+// alert('"안녕하세요."\n사이트 점검 중입니다.\n불편을 드려서 죄송합니다.');//줄바꿈
+// alert("총 인원은 "+(5+2)+"명입니다.");//문자 자료형 과 숫자 자료형의 차이
+// console.log(true > false);// true == 1, false == 0 그래서 true > false 는 true 반환된다.
+// console.log('가위' > '바위');// 한글 아스키코드로 비교연산
+// console.log( 30 > 20 > 10 ); //옆의 계산은 false다. 30 > 20 이 먼저 계산되고 이건 true니까 1이고  1 > 10 은 당연히 false인것.  핵심은 자바스크립트는 위에서 아래로 왼에서 오로 계산 한다는것.
+
+
+
+
+/*
+if 가정문 혹은 조건문 표기방법
+전제: 조건문이 true일때 실행.
+
+예)
+if (점심시간)  { 밥을 먹는다. }
+if(가정 혹은 조건문){
+실행문
+}
+*/
+// if(true){
+//   console.log('오늘은 월요일 입니다.');
+// }else{
+//   console.log('오늘은 월요일이 아닙니다.');
+// }
+
+// if( 300 > 50 ){
+//   alert("300은 50보다 큽니다.");
+//   alert("정답입니다.")
+// }
+
+
+
+
+//
+// 연습)
+// if(true){
+//   console.log('cigar is unhealty.');
+// }else{
+//   console.log('cigar is healty.');
+// }
+//
+// if(true){
+//   alert('good');
+//   alert('day');
+// }else{
+//   alert('bad day')
+// }
